63fd6e6e3bebab06dc8d2db3b4e98d3548414146
Joshua <joshjoshuap1@gmail.com>
2022-07-17 18:04:57 +0800

    chore: configure meta, links

---
+++ src/pages/_app.js
@@ -0,0 +1,24 @@
+import '/src/styles/main.css';
+import Head from 'next/head';
+
+function MyApp({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>Personal Web Resources | JoshJosh </title>
+
+        <meta name="title" content="Insight Market API | Josh Joshua" />
+        <meta
+          name="description"
+          content="My personal project, Its all about market price of crypto nad stocks. ALso include weather api and others."
+        />
+        <meta name="keywords" content="Insight Market API, joshjoshuap, Joshua Pautanes" />
+        <meta name="author" content="Joshua Pautanes" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"></meta>
+      </Head>
+      <Component {...pageProps} />
+    </>
+  );
+}
+
+export default MyApp;
