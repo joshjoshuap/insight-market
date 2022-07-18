0385a864656442f4d8ca59a43b023362fc67304c
Joshua <joshjoshuap1@gmail.com>
2022-07-18 14:20:51 +0800

    styles: configure colors & add google font

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang="en">
       <Head>
         {/* Google Font */}
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Rubik:wght@300;400;700&display=swap"
+          rel="stylesheet"></link>
       </Head>
       <body>
         <Main />
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@ module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
+    colors: {
+      'black': '#121212',
+      'white': '#F9F9F9',
+      'grey': {
+        'light': '#ECECEB',
+        DEFAULT: '#373A40',
+        'dark': '#1B1A17'
+      },
+      'red': {
+        'light': '#EB4747',
+        DEFAULT: '#F32424',
+        'dark': '#B22727'
+      },
+      'blue': {
+        'light': '#3AB4F2',
+        DEFAULT: '#0078AA',
+        'dark': '#187498'
+      },
+      'green': {
+        'light': '#6BCB77',
+        DEFAULT: '#3EC70B',
+        'dark': '#357C3C'
+      },
+    }
   },
   plugins: [],
 }
