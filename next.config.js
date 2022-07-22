393dea564530353f0d83353427e665bf2dd797d3
Joshua <joshjoshuap1@gmail.com>
2022-07-22 17:03:23 +0800

    chore: configure and add icons

--- next.config.js
+++ next.config.js
@@ -3,15 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   images: {
-    domains: ['cdn.weatherapi.com'],
-  },
-  async rewrites() {
-    return [
-      {
-        source: '/api/:path*',
-        destination: 'https://pro-api.coinmarketcap.com/:path*',
-      },
-    ];
+    domains: ['cdn.jsdelivr.net'],
   },
 };
 
--- src/pages/sections/CryptoTable.js
+++ src/pages/sections/CryptoTable.js
@@ -1,7 +1,13 @@
+import Image from 'next/image';
+
 const CryptoCoin = (info) => {
+  let icon = `https://cdn.jsdelivr.net/gh/atomiclabs/cryptocurrency-icons@bea1a9722a8c63169dcc06e86182bf2c55a76bbc/svg/color/${info.symbol.toLowerCase()}.svg`;
   return (
     <tr>
-      <th></th>
+      <th>
+        <Image className='bg-grey-light rounded-full' src={icon} width={40} height={40} alt="" />
+      </th>
+      <th>{info.rank}</th>
       <th>{info.symbol}</th>
       <th>{info.name}</th>
       <th>{info.price_usd}</th>
