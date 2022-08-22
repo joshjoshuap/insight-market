a6ef22398de8c81d1c4ed8d8670c638ab11f6b90
Joshua <joshjoshuap1@gmail.com>
2022-08-22 16:30:53 +0800

    chore: change header description

--- src/pages/sections/Header.js
+++ src/pages/sections/Header.js
@@ -4,8 +4,7 @@ const Header = () => {
       <div>
         <h1 className="text-black text-7xl font-semibold mb-8">Insight Market</h1>
         <p className=" text-black text-xl mb-8">
-          Be updated from trends, and price. Your all in one website to cover all your need as
-          trader or investor.
+         Market price of crypto exchange.
         </p>
         <button className="text-xl font-medium p-2 border-2 rounded cursor-pointer hover:bg-black hover:text-white hover:border-black transition-all duration-300 ease-in">
           <a href="#market">View Market</a>
