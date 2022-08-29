e219ff9e619db23abdc8a82fb705e7970c6d8178
Joshua <joshjoshuap1@gmail.com>
2022-08-29 15:33:32 +0800

    test: testing coinmarket api cors error

--- src/pages/sections/Crypto.js
+++ src/pages/sections/Crypto.js
@@ -5,10 +5,32 @@ const Crypto = () => {
   const [cryptoData, setCryptoData] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const res = await fetch('https://api.coinlore.net/api/tickers/'); // fethcing coinlore api
+  //       const data = await res.json();
+  //       setCryptoData(data);
+  //       setLoading(false);
+  //     } catch (e) {
+  //       console.error('Error', e);
+  //     }
+  //   };
+
+  //   fetchData();
+  // }, []);
+  //localhost:3000/
+  https: useEffect(() => {
     const fetchData = async () => {
       try {
-        const res = await fetch('https://api.coinlore.net/api/tickers/'); // fethcing coinlore api
+        const res = await fetch(
+          'https://sandbox-api.coinmarketcap.com/v1/cryptocurrency/listings/latest',
+          {
+            headers: {
+              'X-CMC_PRO_API_KEY': 'b54bcf4d-1bca-4e8e-9a24-22ff2c3d462c',
+            },
+          }
+        ); // fethcing coinlore api
         const data = await res.json();
         setCryptoData(data);
         setLoading(false);
@@ -24,7 +46,13 @@ const Crypto = () => {
     return <h1>Loading...</h1>; // display if fetch api not available or still fetching
   } else {
     // render CryptoTable and pass fetched api through cryptoData
-    return <div id="market">{<CryptoCard datas={cryptoData} />}</div>;
+    // return <div id="market">{<CryptoCard datas={cryptoData} />}</div>;
+    console.log(cryptoData);
+    return (
+      <>
+        <h1>Done</h1>
+      </>
+    );
   }
 };
 
