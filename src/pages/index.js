84d7cac93270a3c7239bbecd93863438559daad5
Joshua <joshjoshuap1@gmail.com>
2022-09-21 16:21:52 +0800

    chore: add footer, fix footer

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,13 @@
 import Header from './sections/Header';
 import Crypto from './sections/Crypto';
+import Footer from '../components/Footer';
 
 export default function Home() {
   return (
     <>
       <Header />
-      <Crypto/>
+      <Crypto />
+      <Footer />
     </>
   );
 }
