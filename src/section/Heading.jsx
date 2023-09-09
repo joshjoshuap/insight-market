850587a351b072998e8db80fef4d278d858b2857
Joshua <joshjoshuap1@gmail.com>
2023-09-09 16:08:39 +0800

    chore: chore: adjust headding padding & image

--- src/section/Heading.jsx
+++ src/section/Heading.jsx
@@ -2,15 +2,15 @@ import headingImage from "../assets/heading-image.png";
 
 export default function Heading() {
   return (
-    <heading className="flex flex-col items-center text-white py-16 w-10/12 xl:w-full mx-auto">
-      <h1 className="text-4xl md:text-5xl xl:text-8xl text-center font-vastShadow text-cyan mb-6">
+    <heading className="flex flex-col items-center text-white py-8 sm:py-16 w-10/12 xl:w-full mx-auto">
+      <h1 className="text-4xl md:text-5xl xl:text-8xl text-center font-vastShadow text-cyan mb-2 sm:mb-6">
         INSIGHT MARKET
       </h1>
       <h2 className="text-xl md:text-2xl xl:text-4xl font-josefinSans">
         Your Market price for crypto
       </h2>
       <img
-        className="w-10/12"
+        className="hidden sm:block w-46"
         src={headingImage}
         alt="Crypto Icons"
       ></img>
