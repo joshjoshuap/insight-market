4fc0f5e182c691bc6da74f3ea4491f91d1f355a1
Joshua <joshjoshuap1@gmail.com>
2023-09-09 15:56:59 +0800

    chore: heading responsive

--- src/section/Heading.jsx
+++ src/section/Heading.jsx
@@ -2,19 +2,19 @@ import headingImage from "../assets/heading-image.png";
 
 export default function Heading() {
   return (
-    <heading className="flex flex-col items-center text-white py-16">
-      <h1 className="text-8xl text-heading font-vastShadow text-cyan mb-6">
+    <heading className="flex flex-col items-center text-white py-16 mx-auto">
+      <h1 className="text-4xl md:text-5xl xl:text-8xl text-center font-vastShadow text-cyan mb-6">
         INSIGHT MARKET
       </h1>
-      <h2 className="text-4xl font-josefinSans">
+      <h2 className="text-xl md:text-2xl xl:text-4xl font-josefinSans">
         Your Market price for crypto
       </h2>
       <img
-        className="w-fit"
+        className="w-10/12"
         src={headingImage}
         alt="Crypto Icons"
       ></img>
-      <p className="text-sm font-poppins italic font-secondary">
+      <p className="text-xs text-center md:text-sm font-poppins italic font-secondary">
         API based crypto market, shows price, volume and percentage changed
       </p>
     </heading>
