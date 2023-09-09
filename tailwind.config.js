5b1ca6fc97af78781a891e56299b096dbf87e7b5
Joshua <joshjoshuap1@gmail.com>
2023-09-09 12:07:40 +0800

    chore: fix tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,20 +2,22 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
-    fontFamily: {
-      heading: ["Josefin Sans", "sans-serif"],
-      primary: ["Poppins", "sans-serif"],
-      secondary: ["Vast Shadow", "serif"],
+    extend: {
+      colors: {
+        transparent: "transparent",
+        white: "#F5F5F5",
+        black: "#040D12",
+        gray: "#1C2B2D",
+        cyan: "#00FFAB",
+        green: "#A2FF86",
+        red: "#ED2B2A",
+        silver: "#D2E9E9",
+      },
     },
-    colors: {
-      transparent: "transparent",
-      black: "#040D12",
-      gray: "#1C2B2D",
-      cyan: "#00FFAB",
-      green: "#A2FF86",
-      red: "#ED2B2A",
-      silver: "#D2E9E9",
+    fontFamily: {
+      josefinSans: ["Josefin Sans"],
+      poppins: ["Poppins"],
+      vastShadow: ["Vast Shadow"],
     },
   },
   plugins: [],
